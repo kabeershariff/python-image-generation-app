c6209fd5dbeb9be73fe8c3ae064d0e80c016e30a
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2023-11-14 18:36:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
 from kivymd.uix.imagelist import MDSmartTile
+import subprocess
 
 class MainScreen(Screen):
     slider_steps_txt2img = ObjectProperty()
@@ -20,17 +21,40 @@ class MainScreen(Screen):
     img2img_negative_prompt = ObjectProperty()
     img2img_path = ObjectProperty()
 
-    def start_txt2img():
-        pass
+    def start_txt2img(self):
+        steps = int(self.slider_steps_txt2img.value)
+        cfg = int(self.slider_cfg_txt2img.value)
+        amount = int(self.slider_amount_txt2img.value)
+        prompt = self.txt2img_prompt.text
+        remove = self.txt2img_negative_prompt.text
 
-    def start_img2img():
-        pass
+        script_name = "img_generate.py"
+        script_args = ['--prompt', f'"{prompt}"','--remove', f'{remove}', '--amount', f'{amount}', '--steps', f'{steps}', '--cfg', f'{cfg}']
+        Main_functions().command(script_name, script_args)
+
+    def start_img2img(self):
+        steps = int(self.slider_steps_img2img.value)
+        cfg = int(self.slider_cfg_img2img.value)
+        denoise = self.slider_denoise_img2img.value
+        amount = int(self.slider_amount_img2img.value)
+        prompt = self.img2img_prompt.text
+        remove = self.img2img_negative_prompt.text
+        image = self.img2img_path.text
+
+        script_name = "img2img_generate.py"
+        script_args = ['--prompt',f'"{prompt}"', '--remove', f'"{remove}', '--image', f'{image}', '--amount', f'{amount}', '--steps', f'{steps}', '--cfg', f'{cfg}', '--denoise', f'{denoise}']
+        Main_functions().command(script_name, script_args)
     
     def load_images():
         pass
 
     def clear_images():
         pass
+
+class Main_functions:
+    def command(self, script, script_arguments):
+        print("running command")
+        subprocess.call(['python', script] + script_arguments)
 
 class user_interfaceApp(MDApp):
     def build(self):
